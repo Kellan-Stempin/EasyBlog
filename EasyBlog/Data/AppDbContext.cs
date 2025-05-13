b5f5b2855b61930ed7d792680f12fcbcfab44b27
Kellan-Stempin <stempinkellan@gmail.com>
2025-05-12 21:14:34 -0600

    Add project files.

---
+++ EasyBlog/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EasyBlog.Models;
+
+namespace EasyBlog.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ EasyBlog/Models/UserApp.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyBlog.Models
+{
+    // inherits everything from IdentityUser: Id, Email, Password, etc.
+    public class UserApp : IdentityUser
+    {
+        // can add custom properties here
+        // ex: public string DisplayName { get; set; }
+    }
+}
